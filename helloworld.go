1a4a2bed9dddaca894b734646ca24d8d948d4a7d
Romén Rodríguez-Gil <romen@romenrg.com>
2019-09-27 19:08:30 +0100

    Now writes version and adds CRLF in console message

--- helloworld.go
+++ helloworld.go
@@ -10,11 +10,12 @@ func main() {
   httpServer := http.NewServeMux()
   httpServer.HandleFunc("/", sayHello)
   port := "8080"
-  fmt.Printf("Starting server on port %s ...", port)
+  fmt.Printf("Starting server on port %s ...\n", port)
   error := http.ListenAndServe(":"+port, httpServer)
   log.Fatal(error)
 }
 
 func sayHello(writer http.ResponseWriter, request *http.Request) {
   fmt.Fprintln(writer,"Hello world from go!")
+  fmt.Fprintln(writer,"> V.2.0.0")
 }
